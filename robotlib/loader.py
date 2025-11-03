1cd39c56f0a2907d2f6f29d73bda63fe98e314c4
Zaterval <ziang_zheng@foxmail.com>
2025-11-03 13:44:33 +0800

    add g1 loading

--- robotlib/loader.py
+++ robotlib/loader.py
@@ -2,6 +2,10 @@ from typing import Tuple, List, Literal
 
 def load_robot_cfg(robot_type: str)->Tuple[dict, dict]:
 
+    if robot_type == "g1_29d" or robot_type == "r2b":
+        from robotlib.beyondMimic.robots.g1 import G1_CYLINDER_CFG
+        from robotlib.trackerLab.align_cfg import G129DOF_MOTION_ALIGN_CFG_GMR
+        return G1_CYLINDER_CFG, G129DOF_MOTION_ALIGN_CFG_GMR
     if robot_type == "r2" or robot_type == "r2b":
         from robotlib.trackerLab.assets.humanoids.r2 import R2_CFG
         from robotlib.trackerLab.align_cfg import R2B_MOTION_ALIGN_CFG_GMR
